a328c88658186defd9d6b078d7a0ff03210c9c92
Julian Linn <julian404@icloud.com>
2018-11-14 00:55:00 +0100

    Removed ///summary Added new del/add Methodes

--- Liste.cpp
+++ Liste.cpp
@@ -12,26 +12,31 @@ CListe::CListe(CBauelement* _bauelement)
 
 CListe::~CListe()
 {
-	if (start != NULL)
-	{
-		delete start;
-	}
-}
+	CBauelement* ptr = start;
+	int Entrys = this->getEntryCount();
+	CBauelement** delList = new CBauelement*[Entrys];
 
+	for (int i = 0; i < Entrys; i++)
+	{
+		delList[i] = ptr;
+		ptr = ptr->getNext();
+	}
+	for (int i = 0; i < Entrys; i++)
+	{
+		delete	delList[i];
+	}
+	delete[] delList;
+}
 
 CBauelement* CListe::getStart(void) const
 {
 	return start;
 }
 
-///<summary>
-//Returns 0 on success
-///</summary>
 bool CListe::setStart(CBauelement* _newStartPtr)
 {
 	return 0;
 }
-
 
 bool CListe::addToStart(CBauelement* _newStartPtr)
 {
@@ -47,6 +52,79 @@ bool CListe::addToStart(CBauelement* _newStartPtr)
 	}
 }
 
+bool CListe::addToEnd(CBauelement * _newStartPtr)
+{
+	if (_newStartPtr == NULL)
+		return 1;
+	else
+	{
+		if (start == NULL)
+		{
+			start = _newStartPtr;
+		}
+		else
+		{
+			CBauelement* lookupPtr = start;
+			CBauelement* lookupPtrOld = lookupPtr;
+			while (lookupPtr != NULL)
+			{
+				lookupPtrOld = lookupPtr;
+				lookupPtr = lookupPtr->getNext();
+			}
+			lookupPtrOld->setNext(_newStartPtr);
+			return 0;
+		}
+	}
+}
+
+bool CListe::delAtStart()
+{
+	return 0;
+}
+
+bool CListe::delAtEnd()
+{
+	//Check if Entry exist
+	if (start == NULL)
+	{
+		return 1;
+	}
+	CBauelement* nullCopyPtr = NULL;
+	CBauelement* delPtr = NULL;
+	CBauelement* ptr = start;
+
+	/* Need Implementation of Delete Funcionaliti
+	while (ptr != NULL)
+	{
+		ptr = ptr->getNext();
+		)
+		delPtr = ptr;
+		
+	}
+	if (nullCopyPtr != NULL)
+	{
+		nullCopyPtr->setNext(NULL);
+	}
+	if (delPtr != NULL)
+		delete	delPtr;
+	delete delPtr;
+	return 0;
+	*/
+		return 0;
+}
+
+int CListe::getEntryCount(void)const
+{
+	CBauelement* ptr = start;
+	int count = 0;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->getNext();
+	}
+	return count;
+}
+
 void CListe::printAllElements(void) const
 {
 	CBauelement* ptr = NULL;
@@ -57,7 +135,7 @@ void CListe::printAllElements(void) const
 	}
 }
 
-CBauelement* CListe::searchName(std::string _nameToFind) const
+CBauelement* CListe::searchName(const std::string& _nameToFind) const
 {
 	CBauelement* nameSearchPtr = start;
 	while (nameSearchPtr != NULL)
@@ -71,4 +149,14 @@ CBauelement* CListe::searchName(std::string _nameToFind) const
 	return NULL;
 }
 
-
+std::ostream& operator<<(std::ostream& stream, const CListe& _list)
+{
+	CBauelement* ptr = _list.start;
+	while (ptr != NULL)
+	{
+		stream << (*ptr);
+		stream << std::endl;
+		ptr = ptr->getNext();
+	}
+	return stream;
+}
--- Liste.h
+++ Liste.h
@@ -11,49 +11,41 @@ public:
 	CListe();
 	CListe(CBauelement* _bauelement);
 	~CListe();
+	
 	CBauelement* CListe::getStart(void) const;
-
-	///<summary>
-	//Returns 0 on success
-	///</summary>
 	bool setStart(CBauelement* _newStartPtr);
-
-	///<summary>
-	//Returns 0 on success
-	///</summary>
 	bool addToStart(CBauelement* _newStartPtr);
+	bool addToEnd(CBauelement* _newStartPtr);
+	bool delAtStart(void);
+	bool delAtEnd(void);
 
-	///<summary>
-	//Prints all entrys beginning with "start" ptr
-	///</summary>
+	int  getEntryCount(void)const;
 	void printAllElements(void) const;
 
-	///<summary>
-	//Lookup string in name attributes of list members
-	///</summary>
-	CBauelement* searchName(std::string _nameToFind) const;
-
-	///<summary>
-	//Search for specific type of attribute by fktPtr, returns pointer if Element is found.
-	///</summary>
+	CBauelement* searchName(const std::string& _nameToFind) const;
 	template<class T, class K>
-	CBauelement* search(T _searchObj, T (*fktPtr)(K&)) const;
+	CListe* search(T _searchObj, T (*fktPtr)(K&)) const;
+
+	friend std::ostream& operator<<(std::ostream& stream, const CListe& _list);
 };
 
 
 //Will move to .cpp with polymorphie
 template<class T, class K>
-CBauelement* CListe::search(T _searchObj, T(*fktPtr)(K&)) const
+CListe* CListe::search(T _searchObj, T(*fktPtr)(K&)) const
 {
+	static CListe* listOfItemsToReturn = new CListe;
 	CBauelement* searchPtr = start;
 	while (searchPtr != NULL)
 	{
 		if ((*fktPtr)(*searchPtr) == _searchObj)
 		{
-			return searchPtr;
+			CBauelement* newEntry = new CBauelement(*searchPtr);
+			listOfItemsToReturn->addToStart(newEntry);
 		}
 		searchPtr = searchPtr->getNext();
 	}
-	return NULL;
+	return listOfItemsToReturn;
 }
+
 
