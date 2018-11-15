7385d6ba26a14d415b3094b8ebdb3039288bc892
Julian Linn <julian404@icloud.com>
2018-11-15 11:20:33 +0100

    Cleaned Main

--- main.cpp
+++ main.cpp
@@ -32,32 +32,13 @@ int main()
 	BauelementListe->addToEnd(new CSpule("L2", 5.3, CPunkt(17, 4), 0.150));
 
 
-	BauelementListe->printAllElements();
-	std::cout << std::endl;
-	std::cout << "Suche Bauteil mit namen R1:" << std::endl;
-	if (BauelementListe->searchName("R1") != NULL)
-	{
-		std::cout << *BauelementListe->searchName("R1") << std::endl;
-	}
-
-	std::cout << "--------------------------------" << std::endl;
-	std::cout << "Delete Last Element" << std::endl;
-	BauelementListe->delAtEnd();
-	std::cout << "--------------------------------" << std::endl;
+	std::cout << "Widerstandwerte: " << std::endl;
+	
 
 
-	std::cout << "--------------------------------" << std::endl;
-	std::cout << "PrintAllElements()" << std::endl;
-	std::string checkName = "C1";
-	BauelementListe->search(checkName, &getName)->printAllElements();
 
-	std::cout << "--------------------------------" << std::endl; 
-	std::cout << "<< Overloaded" << std::endl;
-	std::cout << *BauelementListe->search(checkName, &getName) << std::endl;
 
-	std::cout << *BauelementListe->search(std::string("R1"), &getName) << std::endl;
 
-	delete BauelementListe;
 	
 
 	//----------------
@@ -65,3 +46,58 @@ int main()
 	system("pause");
 	return 0;
 }
+
+
+/*
+
+
+
+
+// std::cout << BauelementListe->search(std::string("R1"), &getName).getStart() << std::endl;
+
+
+std::cout << "-- .printAllElements() --" << std::endl;
+BauelementListe->searchName("R4");
+std::cout << "-------------------------------------" << std::endl;
+
+
+std::cout << "-- .printAllElements() --"<< std::endl;
+BauelementListe->printAllElements();
+std::cout << std::endl;
+
+std::cout << "-- << overloaded --" << std::endl;
+std::cout << *BauelementListe << std::endl;
+std::cout << std::endl;
+
+
+
+std::cout << "Suche Bauteil mit namen R1:" << std::endl;
+if (BauelementListe->searchName("R1") != NULL)
+{
+std::cout << *BauelementListe->searchName("R1") << std::endl;
+}
+std::cout << "Suche Bauteile mit namen R1 (Return CListe)" << std::endl;
+if (BauelementListe->searchName("R1") != NULL)
+{
+CListe listsearch;
+//		std::cout << BauelementListe->search(std::string("R1"), &getName).getStart() << std::endl;
+
+
+//std::cout << BauelementListe->search(std::string("R1"), &getName) << std::endl;
+}
+
+std::cout << "--------------------------------" << std::endl;
+std::cout << "Delete Last Element" << std::endl;
+BauelementListe->delAtEnd();
+std::cout << "--------------------------------" << std::endl;
+
+
+//	std::cout << BauelementListe->search(std::string("R1"), &getName) << std::endl;
+
+delete BauelementListe;
+
+
+
+
+
+*/
