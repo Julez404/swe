8f0de865260c68a5dd59d22d73e1041594ad24e5
Julian Linn <julian404@icloud.com>
2018-11-24 10:47:59 +0100

    Schaltungsabhängige Impedanzberechnung (entwurfsmuster)

--- ImpedanzRechner.h
+++ ImpedanzRechner.h
@@ -9,7 +9,6 @@ class ImpedanzRechner
 public:
 	ImpedanzRechner();
 	virtual ~ImpedanzRechner();
-
 	virtual CComplex berechneImpedanz(const CListe& _list, const double _f) const = 0;
 };
 
--- main.cpp
+++ main.cpp
@@ -9,30 +9,36 @@
 #include "Spule.h"
 #include "ImpedanzRechner.h"
 #include "ImpedanzRechner_Serie.h"
+#include "ImpedanzRechner_Parallel.h"
 #include "Liste.h"
-
-
-std::string getName(const CBauelement& _bauelement);
-std::string getName(const CBauelement& _bauelement)
-{
-	return _bauelement.getName();
-}
 
 int main()
 {
+	/*
+	DEFINITIONEN
+	*/
+	ImpedanzRechner* reihe = new ImpedanzRechner_Serie;
+	ImpedanzRechner* parallel = new ImpedanzRechner_Parallel;
 
 	CListe* BauelementListe = new CListe;
-	BauelementListe->setImpPtr(new ImpedanzRechner_Serie);
+	BauelementListe->setImpPtr(reihe);
 
+	/*
+	Liste füllen
+	*/
 	BauelementListe->addToEnd(new CWiderstand("R1", 0.35, CPunkt(3, 8), 1.5));
 	BauelementListe->addToEnd(new CSpule("L1", 0.81, CPunkt(12, 2), 0.03));
 	BauelementListe->addToEnd(new CKap("C1", 0.15, CPunkt(3, 18), 0.000270));
 
-	std::cout << BauelementListe->getImpedanzOfList(50) << std::endl;
 
+	/*
+	Auswertung
+	*/
+	BauelementListe->printAllElements();
 
+	
 
-	//----------------
+	//----------------	
 	std::cout << std::endl;
 	system("pause");
 	return 0;
